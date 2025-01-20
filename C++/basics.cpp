51f6b0371ea0fd40d5f187dffd0f8eefdbbfe8c8
KSGL Rohith <22bk1a1250@stpetershyd.com>
2025-01-20 21:54:27 +0530

    Going with basics

---
+++ C++/basics.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    /*cout<<"Hello world"<<endl;//endl will print int he new line
+    cout<<"NExt Line";multi line comments
+    */
+   //variables
+//    int a,b,c;
+//    short sa=9;
+//    cout<<sa;
+
+//camelCase
+//    int marksInMarks=83;
+//    cout<<"The marks : "<<marksInMarks;
+
+// short a;
+// int b;
+// long c;
+// float d;
+// double e;
+// long double f;
+
+cout<<"Hello World";
+
+    return 0;
+}
